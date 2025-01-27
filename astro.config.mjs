f00fbb3f7df52623caec5a1a09ba3ff6580bb011
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2025-01-27 09:10:16 +0100

    feat: changing astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,15 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jobtrek.github.io/', // TODO: Change to your github url
-    base: 'progress-report', // TODO: Change to your repo name
+    site: 'https://murzikkot978.github.io/', // TODO: Change to your github url
+    base: 'Rapport-de-formation', // TODO: Change to your repo name
     integrations: [starlight({
         title: 'Progress  report',
         social: {
-            github: 'https://github.com/jobtrek/progress-report', // TODO: Change to your repo url
+            github: 'https://github.com/murzikkot978/Rapport-de-formation', // TODO: Change to your repo url
         },
         editLink: {
-            baseUrl: 'https://github.com/jobtrek/progress-report/edit/main/docs/', // TODO: before /edit, change to your repo url
+            baseUrl: 'https://github.com/murzikkot978/Rapport-de-formation/edit/main/docs/', // TODO: before /edit, change to your repo url
         },
         customCss: [
             // Relative path to your custom CSS file
